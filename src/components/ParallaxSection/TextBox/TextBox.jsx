61e70fb39a975603d04798a693887373c375e3a2
ClockWize171 <jupiterminhtin998@gmail.com>
2022-12-04 21:32:21 +0630

    minor fixes

--- src/components/ParallaxSection/TextBox/TextBox.jsx
+++ src/components/ParallaxSection/TextBox/TextBox.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 const TextBox = ({ children }) => {
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
     return (
         <div className='text-center bg-gray100'>
-            <h2 style={{ lineHeight: '4rem' }} className='font-montserrat text-3xl lg:text-4xl p-6 '>
+            <h2
+                style={isTabletOrMobile ? { lineHeight: '2.5rem' } : { lineHeight: '4rem' }}
+                className='font-montserrat text-2xl lg:text-4xl p-6 '>
                 {children}
             </h2>
         </div>
