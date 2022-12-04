68d41e742d942861f449161183b58450d21c42ac
ClockWize171 <jupiterminhtin998@gmail.com>
2022-12-04 21:19:16 +0630

    adjust home page textbox

--- src/components/ParallaxSection/TextBox/TextBox.jsx
+++ src/components/ParallaxSection/TextBox/TextBox.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const TextBox = ({children}) => {
+const TextBox = ({ children }) => {
     return (
         <div className='text-center bg-gray100'>
-            <h2 className='font-montserrat text-3xl lg:text-4xl p-6 leading-relaxed'>
+            <h2 style={{ lineHeight: '4rem' }} className='font-montserrat text-3xl lg:text-4xl p-6 '>
                 {children}
             </h2>
         </div>
