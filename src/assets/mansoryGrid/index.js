c0465c86e25299facf007023e06b6f185ff53595
ClockWize171 <jupiterminhtin998@gmail.com>
2022-12-18 17:37:13 +0630

    rename images

--- src/assets/mansoryGrid/index.js
+++ src/assets/mansoryGrid/index.js
@@ -1,17 +1,18 @@
-import img1 from './pexels-david-bartus-586687.jpg'
-import img2 from './pexels-disha-sheta-3584430.jpg'
-import img3 from './pexels-elena-rubtsova-14769376.jpg'
-import img4 from './pexels-julia-sakelli-1011302.jpg'
-import img5 from './pexels-oleksandr-pidvalnyi-345522.jpg'
-import img6 from './pexels-peng-liu-169677.jpg'
-import img7 from './pexels-photo-1701392.jpeg'
-import img8 from './pexels-reign-martinez-11805639.jpg'
-import img9 from './pexels-soubhagya-maharana-5245865.jpg'
-import img10 from './pexels-wendy-wei-14430249.jpg'
+import img1 from './img1'
+import img2 from './img2'
+import img3 from './img3'
+import img4 from './img4'
+import img5 from './img5'
+import img6 from './img6'
+import img7 from './img7'
+import img8 from './img8'
+import img9 from './img9'
+import img10 from './img10'
 
 const imageData = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10]
 let randomImage = imageData
     .map(value => ({ value, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
     .map(({ value }) => value)
+
 export default randomImage;
