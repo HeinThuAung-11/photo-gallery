a10b36577cd32789e003e1ea0a67adbede738a25
ClockWize171 <jupiterminhtin998@gmail.com>
2022-12-18 17:44:04 +0630

    added extension of image

--- src/assets/mansoryGrid/index.js
+++ src/assets/mansoryGrid/index.js
@@ -1,13 +1,13 @@
-import img1 from './img1'
-import img2 from './img2'
-import img3 from './img3'
-import img4 from './img4'
-import img5 from './img5'
-import img6 from './img6'
-import img7 from './img7'
-import img8 from './img8'
-import img9 from './img9'
-import img10 from './img10'
+import img1 from './img1.jpg'
+import img2 from './img2.jpg'
+import img3 from './img3.jpg'
+import img4 from './img4.jpg'
+import img5 from './img5.jpg'
+import img6 from './img6.jpg'
+import img7 from './img7.jpeg'
+import img8 from './img8.jpg'
+import img9 from './img9.jpg'
+import img10 from './img10.jpg'
 
 const imageData = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10]
 let randomImage = imageData
