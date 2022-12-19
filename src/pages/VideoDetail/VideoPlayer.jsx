8e597008c1c630a4f00d6ef2fe83b9b057fe2045
ClockWize171 <jupiterminhtin998@gmail.com>
2022-12-19 19:14:27 +0630

    video player fix

--- src/pages/VideoDetail/VideoPlayer.jsx
+++ src/pages/VideoDetail/VideoPlayer.jsx
@@ -1,12 +1,18 @@
 import Vimeo from '@u-wave/react-vimeo';
-import {type} from "@testing-library/user-event/dist/type";
+import { type } from "@testing-library/user-event/dist/type";
 import ReactPlayer from "react-player";
-export const VideoPlayer=({video})=>{
+
+
+export const VideoPlayer = ({ video }) => {
     let videolink = video?.video_files[0].link;
-   // console.log('videoplayer',video)
-    return <div className={'w-1/2'}>
-        <iframe src={videolink} width="100%"
-                height="500px" frameBorder="0" webkitallowfullscreen={"true"} mozallowfullscreen={"true"}
-                allowFullScreen></iframe>
-    </div>
+    // console.log('videoplayer',video)
+    return (
+        <div className={'w-1/2 m-auto'}>
+            <video className='w-full h-[80vh]' controls autoPlay>
+                <source src={videolink} type="video/mp4"
+                />
+                Sorry, your browser doesn't support videos.
+            </video>
+        </div>
+    )
 }
